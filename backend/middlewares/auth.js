51e67de4a0c9ae6bb7b4859bab681b24ae26dd11
Andrei Iurchenko <iur4.andrej@yandex.ru>
2023-06-13 17:21:29 +0300

    updated

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,23 +1,21 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
+const extractBearerToken = (header) => header.replace('Bearer', '');
+
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
-
-  let payload;
-
-  // if (!authorization || !authorization.startsWith('Bearer ')) {
-  //   return next(new UnauthorizedError('You need to log in'));
-  // }
-
-  // const token = authorization.replace('Bearer ', '');
-
   try {
-    payload = jwt.verify(token, 'gen');
+    const { authorization } = req.headers;
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+      return next(new UnauthorizedError('You need to log in'));
+    }
+
+    const token = extractBearerToken(authorization);
+    const payload = jwt.verify(token, 'gen');
+
+    req.user = payload;
   } catch (err) {
     return next(new UnauthorizedError('You need to log in'));
   }
-
-  req.user = payload;
   return next();
 };
